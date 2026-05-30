ac1f07209eb5b074c4b6d452151d9342c187b670
alex-renny <mr.alex112524@gmail.com>
2026-05-30 09:31:48 +0530

    Nav Logo removed

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -60,13 +60,13 @@ function Navbar() {
         <div className="navbar-container">
           {/* Logo */}
           <a href="#home" className="logo-wrapper" onClick={closeMenu}>
-            <div className="logo-icon">
+            {/* <div className="logo-icon">
               <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <path d="M12 2L2 7l10 5 10-5-10-5z"/>
                 <path d="M2 17l10 5 10-5"/>
                 <path d="M2 12l10 5 10-5"/>
               </svg>
-            </div>
+            </div> */}
             <span className="logo-text">
               <span className="logo-letter">A</span>
               <span className="logo-letter">L</span>
